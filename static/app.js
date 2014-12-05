c9becce49aeec90c158fa5a691362e1466c1322a
Cory <cjkirk09@gmail.com>
2014-12-04 21:58:32 -0700

    added the route box

--- static/app.js
+++ static/app.js
@@ -8,22 +8,37 @@
 			angular.extend($scope, {
 				state: {
 					menuOpen: false,
-					loginOpen: false
+					loginOpen: false,
+					routeBoxOpen: false
 				},
 				userInfo: {
-					username: null,
-					password: null,
-					errorMessage: null
+					username: "",
+					password: "", 
+					errorMessage: "invalid username/password"
+				},
+				routeInfo: {
+					startPoint: "",
+					endPoint: "",
+					path: [], 
+					errorMessage: "ERROR MESSAGE WILL GO HERE"
 				},
 				toggleMenu: function()
 				{
 					$scope.state.menuOpen = !$scope.state.menuOpen;	
 					$scope.state.loginOpen = false;
+					$scope.state.routeBoxOpen = false;
 				},
 				toggleLogin: function()
 				{
 					$scope.state.menuOpen = false;
 					$scope.state.loginOpen = !$scope.state.loginOpen;
+					$scope.state.routeBoxOpen = false;
+				},
+				toggleRouteBox: function()
+				{
+					$scope.state.menuOpen = false;
+					$scope.state.loginOpen = false;
+					$scope.state.routeBoxOpen = !$scope.state.routeBoxOpen;
 				},
 				register: function()
 				{
@@ -32,6 +47,12 @@
 				login: function()
 				{
 					$scope.userInfo.errorMessage = "clicked Login";					
+				},
+				getRoute: function()
+				{
+					//verify that startpoint and endpoint are not empty
+					//infoService.getPath()
+					$scope.routeInfo.errorMessage = "I couldn't get the route yet";
 				}
 
 			});
@@ -41,7 +62,7 @@
 	app.factory('infoService',['$http', function($http)
 	{
 		return {
-			getPosts: function()
+			getPath: function()
 			{
 				return $http.get('http://localhost:3000/posts')
 					.then(function(response) {
